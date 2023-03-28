b75d71ff555618de8f8916e12fd8a46977c471c3
boiv66 <vothanhboiloc2409@gmail.com>
2023-03-28 01:33:48 -0700

    init commit

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithoutCache,clearCacheByEndpoint, loading } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      console.log("new transaction: ", transactions)
+      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+        transactionId,
+        value: newValue,
+      })
+      await clearCacheByEndpoint(["paginatedTransactions","transactionsByEmployee"]);
+      console.log("newValue: ", newValue, transactionId);
+      console.log("new transaction after: ", transactions)
+    },
+    [fetchWithoutCache, clearCacheByEndpoint]
+  )
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
